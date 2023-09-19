e12c3ded08f1c994b8bcf86010c9085c309b46b6
OKUDAYUKI08 <yukidorakue2828@gmail.com>
2023-09-19 18:32:36 +0900

    変更しました。

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+
+    if(!empty($_POST["submitButton"])){
+        echo $_POST["comment"];
+        echo $_POST["username"];
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP掲示板</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 class ="title">PHPで掲示板アプリ</h1>
+    <hr>
+    <div class="boardWrapper">
+        <section>
+            <article>
+                <div class="wrapper">
+                    <div class="nameArea">
+                        <span>名前：</span>
+                        <p class="username">shinCode</p>
+                        <time>:2023/9/15</time>
+                    </div>
+                    <p class="comment">手書きコメントです</p>
+                </div>
+            </article>
+        </section>
+        <form class="formWrapper" method="post">
+            <div>
+                <input type="submit" value="書き込む" name="submitButton">
+                <label for="">名前:</label>
+                <input type="text" name="username">
+            </div>
+            <div>
+                <textarea class="commentTextArea" name="comment"></textarea>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
